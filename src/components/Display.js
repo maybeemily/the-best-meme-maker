b806682ce0d1cce22c62bdddcfa46019ee974d72
emily baier <emilyrbaier@gmail.com>
2019-06-05 17:24:52 -0700

    Memes can be saved to your downloads folder

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Display.css';
 
-export default function Display({ topText, image, bottomText }) {
+export default function Display({ topText, image, bottomText, memeComplete }) {
   return (
     <>
-    <p>{topText}</p>
-    <img src={image}/>
-    <p>{bottomText}</p>
+    <p className={styles.introP}>Make your own meme, advice animal, picture with text... whatever you want to call it. 
+      Paste the URL of the image you&apos;d like to use, type the words you want, and save! 
+      You can download your creation, or add it to our gallery.</p>
+    <div className={styles.container}>
+      <div className={styles.memeDiv} ref={memeComplete}>
+        <div className={styles.top}>
+          <span>{topText}</span>
+        </div>
+        <img src={image}/>
+        <div className={styles.bottom}>
+          <span>{bottomText}</span>
+        </div>
+      </div>
+    </div>
     </>
   );
 }
@@ -14,5 +26,6 @@ export default function Display({ topText, image, bottomText }) {
 Display.propTypes = {
   topText: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-  bottomText: PropTypes.string.isRequired
+  bottomText: PropTypes.string.isRequired,
+  memeComplete: PropTypes.object.isRequired
 };
--- src/containers/Meme.js
+++ src/containers/Meme.js
@@ -2,6 +2,9 @@ import React, { PureComponent } from 'react';
 import Display from '../components/Display';
 import TextSelector from '../components/TextSelector';
 import ImageSelector from '../components/ImageSelector';
+import { saveAs } from 'file-saver';
+import domtoimage from 'dom-to-image';
+
 
 export default class Meme extends PureComponent {
   state ={ 
@@ -10,18 +13,33 @@ export default class Meme extends PureComponent {
     image: ''
   }
 
+  memeRef = React.createRef();
+
+
   handleChange = ({ target }) => {
     this.setState({ [target.name ] : target.value });
   }
 
+  
+
+  saveMeme = event => {
+    event.preventDefault();
+    domtoimage.toPng(this.memeRef.current)
+      .then(img => {
+        saveAs(img);
+      });
+
+  }
+
   render() {
     const { topText, bottomText, image } = this.state;
-
     return (
       <>
-      <Display topText={topText} bottomText={bottomText} image={image} handleChange={this.handleChange}/>
+      <Display topText={topText} bottomText={bottomText} image={image} memeComplete={this.memeRef} handleChange={this.handleChange}/>
       <ImageSelector image={image} handleChange={this.handleChange}/>
-      <TextSelector topText={topText} bottomText={bottomText} handleChange={this.handleChange}/>      </>
+      <TextSelector topText={topText} bottomText={bottomText} handleChange={this.handleChange}/>
+    <button onClick={this.saveMeme}>Save Meme</button>
+      </>
     );
   }
 }
