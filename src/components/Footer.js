ba278d76bcf5fb9c6471b8b422a2cecb8dcadea8
emily baier <emilyrbaier@gmail.com>
2019-06-07 16:03:14 -0700

    updated github links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { InfoFooter } from '../styling/styles';
 export default function Footer() {
   return (
     <InfoFooter>
-      the best meme maker is a fullstack app by <a href="https://github.com/maybeemily?tab=repositories">emily baier</a> (front end) and <a href="https://github.com/chrislupdx">chris lu</a> (back end).<br/>
+      the best meme maker is a fullstack app by <a href="https://github.com/maybeemily">emily baier</a> (front end) and <a href="https://github.com/chrislupdx">chris lu</a> (back end).<br/>
       made using react, styled-components, nodejs, mongoose, express, mongodb.
     </InfoFooter>
   );
